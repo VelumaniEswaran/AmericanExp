a22b5424dadd8110a562792a13cec83d6b681d6f
VelumaniEswaran <velumanitesting2020@gmail.com>
2025-02-12 20:59:30 +0530

    Update ConfigFileReader.java

--- ConfigFileReader.java
+++ ConfigFileReader.java
@@ -8,12 +8,7 @@ import java.util.Properties;
 
 import enums.DriverType;
 import enums.EnvironmentType;
-/**
- * 
- * @author AjanthanSivalingarajah
- * @since 2020/05/12
- *
- */
+
 public class ConfigFileReader {
     private Properties properties;
 
