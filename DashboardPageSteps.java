94eae34c7743b40afa46793cdd9ff4c48fae612f
VelumaniEswaran <velumanitesting2020@gmail.com>
2025-02-12 21:00:28 +0530

    Update DashboardPageSteps.java

--- DashboardPageSteps.java
+++ DashboardPageSteps.java
@@ -5,12 +5,7 @@ import org.hamcrest.MatcherAssert;
 import cucumber.TestContext;
 import io.cucumber.java.en.When;
 import pageObjects.DashboardPage;
-/**
- * 
- * @author AjanthanSivalingarajah
- * @since 2020/05/12
- *
- */
+
 public class DashboardPageSteps {
 
     private TestContext testContext;
